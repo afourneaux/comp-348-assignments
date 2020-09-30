7da658ed588f565af403c8edd3edfed87a0bf5c0
Jean Robatto <robatto.jeanmarie@gmail.com>
2020-09-30 17:21:27 -0400

    Added some tests and more contraints

--- A01/COMP348-A1-AlexJeanFonz/src/main/java/com/jeanrobs/comp348/a1/alexjeanfonz/Employee.java
+++ A01/COMP348-A1-AlexJeanFonz/src/main/java/com/jeanrobs/comp348/a1/alexjeanfonz/Employee.java
@@ -1,6 +1,8 @@
 package com.jeanrobs.comp348.a1.alexjeanfonz;
 
 import java.math.BigDecimal;
+import java.util.Objects;
+import javax.naming.directory.InvalidAttributeValueException;
 
 /**
  * Employee class
@@ -13,10 +15,39 @@ public class Employee implements Person{
     private BigDecimal salary;
     
     //Constants
-    private final static int ID = 0, FIRST_NAME = 1, LAST_NAME = 2, SALARY = 4;
+    private final static int ID = 0, FIRST_NAME = 1, LAST_NAME = 2, SALARY = 3;
+    private final static String isIntRegex = "^\\d+$";
     
     //Constructor
-    public Employee(final String id, final String firstName, final String lastName, final BigDecimal salary) {
+    public Employee(final String id, final String firstName, final String lastName, final BigDecimal salary) throws InvalidAttributeValueException {
+        //Check for null vars
+        Objects.requireNonNull(id);
+        Objects.requireNonNull(firstName);
+        Objects.requireNonNull(lastName);
+        Objects.requireNonNull(salary);
+        
+        //Check for empty vars
+        if (id.isEmpty()) {
+            throw new InvalidAttributeValueException("Id can't be empty");
+        }
+        
+        if (firstName.isEmpty()) {
+            throw new InvalidAttributeValueException("First name can't be empty");
+        }
+        
+        if (lastName.isEmpty()) {
+            throw new InvalidAttributeValueException("Last name can't be empty");
+        }
+        
+        //Check that the id has 7 digits
+        if (id.length() != 7) {
+             throw new InvalidAttributeValueException("Id needs to be 7 digits");
+        }
+        
+        if (!id.matches(isIntRegex)) {
+             throw new InvalidAttributeValueException("Id needs to be only digits");
+        }
+        
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -29,10 +60,15 @@ public class Employee implements Person{
      * @param employeeString  Format: "id,firstname,lastname,salary"
      * @return A constructed employee
      * @throws NumberFormatException if the salary is not a valid number
+     * @throws javax.naming.directory.InvalidAttributeValueException
      */
-    public static Person parse(String employeeString) throws NumberFormatException {
+    public static Person parse(String employeeString) throws NumberFormatException, InvalidAttributeValueException {
         String[] attributes = employeeString.split(",");
         return new Employee(attributes[ID], attributes[FIRST_NAME], attributes[LAST_NAME], new BigDecimal(attributes[SALARY]));
+    }
+    
+    public BigDecimal getSalary() {
+        return this.salary;
     }
     
     //Interface methods
@@ -47,10 +83,9 @@ public class Employee implements Person{
     }
     
     //Object methods
-    //TODO: Finish this method
     @Override
     public String toString() {
-        return "";
+        return String.format("Id: %s, Name: %s %s, Salary: %s", this.id, this.firstName, this.lastName, this.salary);
     }
     
     
--- A01/COMP348-A1-AlexJeanFonz/src/main/java/com/jeanrobs/comp348/a1/alexjeanfonz/Program.java
+++ A01/COMP348-A1-AlexJeanFonz/src/main/java/com/jeanrobs/comp348/a1/alexjeanfonz/Program.java
@@ -9,6 +9,41 @@ public class Program {
     public static void main(String[] args) {
         //Part 1 tests
         
+        //Successful parse
+        String employeeString1 = "4563723,Jean,Robatto,80000";
+        try {
+            Person p = Employee.parse(employeeString1);
+            System.out.println(p);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        
+        //Wrong parse = id too short
+        String employeeString2 = "4723,Jean,Robatto,80000";
+        try {
+            Person p = Employee.parse(employeeString2);
+            System.out.println(p);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        
+        //Wrong parse = id not an int
+        String employeeString3 = "4563df2,Jean,Robatto,80000";
+        try {
+            Person p = Employee.parse(employeeString3);
+            System.out.println(p);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        
+        //Wrong parse = empty strings
+        String employeeString4 = "4563df2,Jean,,80000";
+        try {
+            Person p = Employee.parse(employeeString4);
+            System.out.println(p);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
     
 }

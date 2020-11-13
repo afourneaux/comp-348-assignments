f23d1ce2b93a8c32009016b85c7ee421adb2c112
Jean Robatto <robatto.jeanmarie@gmail.com>
2020-11-12 21:50:49 -0500

    L

--- A02/C/q12/selectionsort.c
+++ A02/C/q12/selectionsort.c
@@ -3,16 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void selectionsort(int toSort[], int size, int* (*min)(int*,int)) {
+void selectionsort(int toSort[], int size, int* (*func)(int*,int)) {
     int bound;
     int* smallest;
     int swap;
-    if (min == NULL) {
+    if (func == NULL) {
         printf("No compare function given.");
         exit(1);
     }
     for (bound = 0; bound < size; bound++) {
-        smallest = min(&toSort[bound], size - bound);
+        smallest = func(&toSort[bound], size - bound);
         swap = toSort[bound];
         toSort[bound] = *smallest;
         *smallest = swap;
